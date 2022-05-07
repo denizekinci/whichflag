bbcfa82106248c90acaec016f5b58c1d3e527d37
Deniz Ekinci <deniz.ekinci.dev@gmail.com>
2022-05-07 19:01:48 +0300

    initial commit

---
+++ precache-manifest.b469f3ad09e6cd42e7bb0b38a794dbac.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e0bad5fb5b5549dc3d5f",
+    "url": "/css/app.922609f3.css"
+  },
+  {
+    "revision": "31b0d6d9ddd581dcd29ccb9b4ba761b5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "29a8dc219044780acc5d",
+    "url": "/js/about.ce4d68ea.js"
+  },
+  {
+    "revision": "e0bad5fb5b5549dc3d5f",
+    "url": "/js/app.00edebac.js"
+  },
+  {
+    "revision": "0da632930f50581b8c88",
+    "url": "/js/chunk-vendors.84673f02.js"
+  },
+  {
+    "revision": "4884a4665ea8b3673f10c921d880e6e8",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
